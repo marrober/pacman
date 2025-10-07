6db1fb634332123b48256477fa0dd4a21f06224f
marrober <marrober@redhat.com>
2025-10-07 18:58:38 +0100

    red

--- src/public/pacman-canvas.js
+++ src/public/pacman-canvas.js
@@ -289,7 +289,7 @@ function geronimo() {
         };
         this.gameOver = false;
         this.canvas = $("#myCanvas").get(0);
-        this.wallColor = "Grey";
+        this.wallColor = "Red";
         this.width = this.canvas.width;
         this.height = this.canvas.height;
 
