16c4e06e6f18b9c6ed5ebb3d40e2f8467c9efc5a
marrober <marrober@redhat.com>
2026-09-17 16:24:38 +0100

    Updates

--- image-git-signing-setup/local-git-signing-remove.sh
+++ image-git-signing-setup/local-git-signing-remove.sh
@@ -1,5 +1,6 @@
 git config --global commit.gpgsign false
 git config --global tag.gpgsign false
+git config --local commit.gpgsign false
 git config --global user.email marrober@redhat.com
 git config --global user.name marrober
 
